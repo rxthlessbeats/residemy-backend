f1bcd7fb3f115cbbde49f35cdd992e026c40c249
rxthlessbeats <lutinyu@gmail.com>
2024-02-21 18:08:02 +0800

    host

--- backend/settings.py
+++ backend/settings.py
@@ -169,7 +169,7 @@ CORS_ORIGIN_WHITELIST = [
      'https://learn.residemy.org',
 ]
 
-ALLOWED_HOSTS = ['140.114.134.26', '127.0.0.1']
+ALLOWED_HOSTS = ['140.114.134.26', '127.0.0.1', 'learn.residemy.org']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
