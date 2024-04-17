26de2683fafc45a895078b782fb68e2675ca9eba
rxthlessbeats <lutinyu@gmail.com>
2024-04-17 21:21:45 +0800

    key

--- backend/settings.py
+++ backend/settings.py
@@ -184,3 +184,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
