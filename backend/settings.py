5e36195ab6d3925191aba367e767f8f35a5b9629
rxthlessbeats <lutinyu@gmail.com>
2024-05-03 17:54:24 +0800

    revise settings

--- backend/settings.py
+++ backend/settings.py
@@ -171,7 +171,7 @@ CORS_ORIGIN_WHITELIST = [
      'https://admin.learn.residemy.org',
 ]
 
-ALLOWED_HOSTS = ['140.114.134.26', '127.0.0.1', 'learn.residemy.org', '0.0.0.0', 'admin.learn.residemy.org', '140.114.134.26']
+ALLOWED_HOSTS = ['192.168.50.26', '127.0.0.1', 'learn.residemy.org', '0.0.0.0', 'admin.learn.residemy.org', '140.114.134.26']
 
 CSRF_TRUSTED_ORIGINS = ['https://learn.residemy.org', 'https://admin.learn.residemy.org']
 
